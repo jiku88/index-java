16f730a6307c4c11aef1ce03825c0edecaf0e98e
jiku88 <jutirmoydatta73@gmail.com>
2023-10-06 22:39:36 +0600

    complite

---
+++ index.js
@@ -0,0 +1,28 @@
+// Addition
+
+var a = 10;
+var b = 5;
+var c = a + b;
+
+console.log("additon c = ", c)
+
+// multipilication
+
+var a = 20;
+var b = 5;
+var c = a*b;
+
+console.log("multipilication c =", c)
+
+//  substraction
+var d = 30;
+var e = 5;
+var f = d-e;
+
+console.log("substraction f =", f)
+
+// division
+var a = 30;
+var b = 5;
+var c = a-b;
+console.log("divition c=", c)
